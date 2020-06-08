f8cde7f1428f947ce6cd2e9a8dbb7dfd717df9ef
root <root@localhost.localdomain>
2020-06-08 10:27:10 +0530

    Updates

---
+++ precache-manifest.e0e262f9d83ee0e4d88415408cccaf41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a06f7ed7b2001daa827c19c0e0030c",
+    "url": "/thedeviner/index.html"
+  },
+  {
+    "revision": "3a7682231e2e3fb9d3ea",
+    "url": "/thedeviner/static/css/main.11d7ffcd.chunk.css"
+  },
+  {
+    "revision": "bbba066cbe785dfbaf3c",
+    "url": "/thedeviner/static/js/2.1a656e96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thedeviner/static/js/2.1a656e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7682231e2e3fb9d3ea",
+    "url": "/thedeviner/static/js/main.de4a7d10.chunk.js"
+  },
+  {
+    "revision": "9b5017cfd6362204f3ff",
+    "url": "/thedeviner/static/js/runtime-main.135994c3.js"
+  },
+  {
+    "revision": "8d406845b07b6a8337f1fdea90306d25",
+    "url": "/thedeviner/static/media/deviner.8d406845.jpg"
+  }
+]);
